46449d3cec56f0b0623f789e1e2ffeef5671b91a
sungjaecho <liadus@naver.com>
2021-12-15 09:27:14 +0900

    서비스생성

--- src/main/java/com/cos/book/service/BookService.java
+++ src/main/java/com/cos/book/service/BookService.java
@@ -1,8 +1,28 @@
 package com.cos.book.service;
 
+import com.cos.book.domain.Book;
+import com.cos.book.domain.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service // 기능을 정의할 수 있고, 트랜잭션을 관리할 수 있음.
+@RequiredArgsConstructor
 public class BookService {
-    // 함수 => 송금() -> 레파지토리에 여러개의 함수 실행 -> commit or rollback
+
+    private final BookRepository bookRepository;
+
+    public Book 저장하기(Book book){
+        return null;
+    }
+
+    public Book 한건가져오기(Long id) {
+        return null;
+    }
+
+    public List<Book> 모두가져오기() {
+        return null;
+    }
+
 }
