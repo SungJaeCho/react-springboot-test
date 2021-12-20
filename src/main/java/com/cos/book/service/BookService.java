9a64b175fafa41d14122bceff6afef10d94f966c
sungjaecho <liadus@naver.com>
2021-12-21 08:44:55 +0900

    서비스 완료

--- src/main/java/com/cos/book/service/BookService.java
+++ src/main/java/com/cos/book/service/BookService.java
@@ -30,11 +30,17 @@ public class BookService {
 
     @Transactional
     public Book 수정하기(Long id, Book book) {
-        return null;
-    }
+        // 더티 체킹 update 치기
+        Book bookEntity = bookRepository.findById(id).orElseThrow(()->new IllegalArgumentException("id를 확인해주세요!")); // 영속화 (book 오브젝트)
+        bookEntity.setTitle(book.getTitle());
+        bookEntity.setAuthor(book.getAuthor());
+        return bookEntity;
+    } // 함수 종료 => 트랜잭션 종료 => 영속화 되어있는 데이터를 DB로 갱신(flush) => commit ===> 더티체킹
 
+    @Transactional
     public String 삭제하기(Long id) {
-        return null;
+        bookRepository.deleteById(id); //오류가 터지면 익셉션을 타니까 신경안써도됨
+        return "ok";
     }
 
 }
