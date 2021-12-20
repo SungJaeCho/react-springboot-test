366de28c880331faceceda74972dafd6b1d49e0a
sungjaecho <liadus@naver.com>
2021-12-20 09:26:12 +0900

    서비스생성중

--- src/main/java/com/cos/book/service/BookService.java
+++ src/main/java/com/cos/book/service/BookService.java
@@ -4,6 +4,7 @@ import com.cos.book.domain.Book;
 import com.cos.book.domain.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,15 +14,26 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
+    @Transactional
     public Book 저장하기(Book book){
-        return null;
+        return bookRepository.save(book);
     }
 
+    @Transactional(readOnly = true) // JPA 변경감지라는 내부 기능 활성화 되지 않아 내부연산 줄임, update시의 정합성유지
     public Book 한건가져오기(Long id) {
-        return null;
+        return bookRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id를 확인해주세요!"));
     }
 
     public List<Book> 모두가져오기() {
+        return bookRepository.findAll();
+    }
+
+    @Transactional
+    public Book 수정하기(Long id, Book book) {
+        return null;
+    }
+
+    public String 삭제하기(Long id) {
         return null;
     }
 
