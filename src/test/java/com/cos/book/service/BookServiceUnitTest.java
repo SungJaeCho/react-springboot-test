bab5c559faef29f17229d8d00a2aaf84679ece5e
sungjaecho <liadus@naver.com>
2021-12-24 09:01:53 +0900

    Junit테스트 작성중

---
+++ src/test/java/com/cos/book/service/BookServiceUnitTest.java
@@ -0,0 +1,6 @@
+package com.cos.book.service;
+
+// 단위 테스트 (Service와 관련된 애들만 메모리에 띄우면 됨)
+
+public class BookServiceUnitTest {
+}
---
+++ src/test/java/com/cos/book/web/BookControllerUnitTest.java
@@ -0,0 +1,10 @@
+package com.cos.book.web;
+
+// 단위 테스트 ( Controller 관련 로직만 띄우기 ) Filter, ControllerAdvice
+// https://howtodoinjava.com/spring-boot2/testing/ 사이트에서 정보 확인가능
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest // 스프링 환경으로 테스트한다는 의미 꼭 넣어야함
+public class BookControllerUnitTest {
+}
