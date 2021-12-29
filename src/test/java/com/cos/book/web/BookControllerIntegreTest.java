994d8350b7c060ce14b2edf148cf0556a4aaec8a
sungjaecho <liadus@naver.com>
2021-12-29 09:04:13 +0900

    단위테스트 실행 단위테스트(컨트롤러)의 경우 service, repositoy가 실제로 메모리에 올리는게 아니다보니 MockBean을 통해 가생성 해준뒤 임의의 데이터를 만들어서 stub을 해주고 그 후 when - then을 통해 검증
    통합테스트의 경우
    모든게 다 메모리에 올라가기때문에 실제환경이랑 같은 테스트가 가능하며
    실제로 db에 입력됨.

--- src/test/java/com/cos/book/web/BookControllerIntegreTest.java
+++ src/test/java/com/cos/book/web/BookControllerIntegreTest.java
@@ -1,12 +1,22 @@
 package com.cos.book.web;
 
+import com.cos.book.domain.Book;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /*
     통합테스트 ( 모든 Bean들을 똑같이 Ioc에 올리고 테스트 하는 것)
@@ -25,9 +35,23 @@ public class BookControllerIntegreTest {
     @Autowired
     private MockMvc mockMvc;
 
+    // BDDMockito 패턴
     @Test
-    public void save_테스트() {
-        log.info("save_테스트() 시작 =====================================");
+    public void save_테스트() throws Exception {
+        // given (테스트를 하기위한 준비)
+        Book book = new Book(null, "스프링따라하기", "코스");
+        String content = new ObjectMapper().writeValueAsString(book);
+        // stub(행동 미리 지정), 통합테스트에서는 필요없음 실제로 작동하기 때문
 
+        // when (테스트 실행)
+        ResultActions resultAction = mockMvc.perform(post("/book")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content)
+                .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        // then (검증)
+        resultAction.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("스프링따라하기")) // JsonPath문법에 따라 처리
+                .andDo(MockMvcResultHandlers.print());
     }
 }
--- src/test/java/com/cos/book/web/BookControllerUnitTest.java
+++ src/test/java/com/cos/book/web/BookControllerUnitTest.java
@@ -12,9 +12,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @WebMvcTest // 스프링 환경으로 테스트한다는 의미 꼭 넣어야함
@@ -23,16 +29,27 @@ public class BookControllerUnitTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean // IOC환경에 bean 등록됨.
+    @MockBean // IOC환경에 bean 등록됨. 단위테스트에서만 필요
     private BookService bookService;
 
     // BDDMockito 패턴
     @Test
-    public void save_테스트() throws JsonProcessingException {
-        log.info("save_테스트() 시작 =====================================");
-        Book book = new Book(null,"스프링따라하기","코스");
+    public void save_테스트() throws Exception {
+        // given (테스트를 하기위한 준비)
+        Book book = new Book(null, "스프링따라하기", "코스");
         String content = new ObjectMapper().writeValueAsString(book);
-        // stub(행동 미리 지정)
-        when(bookService.저장하기(book)).thenReturn(new Book(1L,"스프링따라하기","코스"));
+        // stub(행동 미리 지정) 단위테스트에서만 필요
+        when(bookService.저장하기(book)).thenReturn(new Book(1L, "스프링따라하기", "코스"));
+
+        // when (테스트 실행)
+        ResultActions resultAction = mockMvc.perform(post("/book")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(content)
+                .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        // then (검증)
+        resultAction.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("스프링따라하기")) // JsonPath문법에 따라 처리
+                .andDo(MockMvcResultHandlers.print());
     }
 }
